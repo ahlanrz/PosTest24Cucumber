7d4e476f1d4eced005c88163a59912f9c4b74253
Ahlan Rezki <ahlanrezki@gmail.com>
2022-10-31 19:18:07 +0700

    JC Day 24

---
+++ src/main/java/com/ahlanrezki/cucumber/step_definitions/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.ahlanrezki.cucumber.step_definitions.utils;
+
+public class Constants {
+    public static final int TIMEOUT = 10;
+    public static final String CHROME = "Chrome";
+    public static final String FIREFOX = "Firefox";
+    public static final String URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+//    public static final String URL = "";
+    public static final int DETIK = 1;
+}
---
+++ src/main/java/com/ahlanrezki/cucumber/step_definitions/utils/Utils.java
@@ -0,0 +1,5 @@
+package com.ahlanrezki.cucumber.step_definitions.utils;
+
+public class Utils {
+    public static int testCount = 0;
+}
